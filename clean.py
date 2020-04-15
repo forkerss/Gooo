1f8a18b245b3ee22f2e7fef7f4a35bae40f55e9f
whoami <admin@imipy.com>
2020-04-15 10:06:38 +0800

    fix: Escape of special characters

--- clean.py
+++ clean.py
@@ -1,11 +1,14 @@
 #! /usr/bin/python3
 import os, sys
 
+def sh_escape(s):
+    return s.replace("(","\\(").replace(")","\\)")
+
 def clean(rootpath, cleardirs=["__pycache__"], clearfiles=[".DS_Store"]):
     def remove(root, name):
         cur_path = os.path.join(root, name)
         print("* remove %s" % cur_path)
-        os.system("rm -rf %s" % cur_path)
+        os.system("rm -rf %s" % sh_escape(cur_path))
     
     for root, dirs, files in os.walk(rootpath):
         for name in files:
